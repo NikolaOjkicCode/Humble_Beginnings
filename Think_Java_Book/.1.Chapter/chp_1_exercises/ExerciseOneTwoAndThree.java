87cecbce1f23c456435571222c760dd5ef7b7299
NikolaOjkicCode <69131516+NikolaOjkicCode@users.noreply.github.com>
2022-11-14 17:37:26 +0100

    Add files via upload

---
+++ Think_Java_Book/.1.Chapter/chp_1_exercises/ExerciseOneTwoAndThree.java
@@ -0,0 +1,40 @@
+/*
+1.Create a java file and in it (in comments), answer these questions:
+
+-In computer jargon, what's the diference between a statement and a
+comment?
+-What does it mean to say that a program is portable?
+-In common English, what does the word compile mean?
+-What is an executable? Why is that word used as a noun?
+
+2.Type in the hello world program, then compile and run it.
+
+3.Add a print statement that displays a second message after the Hello, World!". Say something witty like, How are you?" Compile and run the program again.
+
+4.Starting with the hello world program, try out each of the following errors. After you make each change, compile the program, read the error message (if there is one), and then fix the error.
+
+-Remove one of the open squiggly braces.
+-Remove one of the close squiggly braces.
+-Instead of main, write mian.
+-Remove the word static.
+-Remove the word public.
+-Remove the word System.
+-Replace println with Println.
+-Replace println with print.
+-Delete one of the parentheses. Add an extra one.
+*/
+
+public class ExerciseOneTwoAndThree{
+
+  
+  public static void main(String[] arg){
+    
+    System.out.println("Hello world!");
+    System.out.println("Goodbye!");
+    
+    //The A statement is a line of code that performs a basic operation.A comment is a bit of english text that explains the code.
+    //Portable means that the programs can run on diferent kinds of computers with few or no modifications.
+    //Compiling means to trnslate  high-level languages  into a low-level language, also called "machine language".
+    //Executable is a translated program that you can execute repeatedly without further translation.
+  }
+}
