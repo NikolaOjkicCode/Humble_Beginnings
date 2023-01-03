bb75482e3dc0dec54dd2c09cc7d4df2eeec4a7d9
NikolaOjkicCode <69131516+NikolaOjkicCode@users.noreply.github.com>
2023-01-03 14:26:21 +0100

    Add files via upload

---
+++ Think_Java_Book/.1.Chapter/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+	public static void main(String[] arg) {
+		//Comment
+		System.out.println("Hello Wolrd!"); 
+		System.out.print("Goodbye, ");//print unlike println does not add a new line on the end of a string
+		System.out.println("cruel world!"); 
+		System.out.println();
+		
+		//Using backslash allows you to break line,add " and \ and also tabs
+		System.out.println("How are you \ntoday \nis everything \"ok?\"\nI am doing \t\\GOOD\\"); 
+	}
+}
