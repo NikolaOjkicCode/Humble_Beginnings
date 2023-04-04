7a24d41c55a7927b6277c402ac94201c3dd168b2
NikolaOjkicCode <69131516+NikolaOjkicCode@users.noreply.github.com>
2023-04-04 15:26:47 +0200

    Add files via upload

---
+++ Kurs_JavaWebProgramiranje/Termin01/Zadaci/Zadatak_1.java
@@ -0,0 +1,27 @@
+package java_i_okruzenje;
+
+public class Zadatak_1 {
+
+	public static void main(String[] args) {
+		
+		/*1.Ispisati na konzoli sledeći tekst (primena konstrukcije za uvlačenje teksta)
+		 
+			A. 
+			B. 
+				B.1. 
+					B.1.1. 
+				B.2.
+			C.
+			
+		*/
+		
+		System.out.println("A.");
+		System.out.println("B.");
+		System.out.println("	B.1");
+		System.out.println("		B.1.1.");
+		System.out.println("	B.2");
+		System.out.println("C.");
+
+	}
+
+}
